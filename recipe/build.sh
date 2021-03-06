d8a51572b691c63bb62d26736f4610e699e43fb3
Kyle Gorman <kylebgorman@gmail.com>
2021-03-05 22:28:25 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,6 @@
+# Removes -std=c++XX from build ${CXXFLAGS}.
+export CXXFLAGS=$(echo "${CXXFLAGS}" | sed -E 's@-std=c\+\+[^ ]+@@g')
+
 ./configure --prefix="${PREFIX}"
 make -j"${CPU_COUNT}"
 make -j"${CPU_COUNT}" install
